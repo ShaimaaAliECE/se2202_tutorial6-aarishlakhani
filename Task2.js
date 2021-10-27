0f369ee7dc0e849bfe8df9cbcb9ee6e796aa41fb
Aarish <aarishlakhani2@gmail.com>
2021-10-26 22:24:11 -0400

    Tutorial 6 Task 2

--- Task2.js
+++ Task2.js
@@ -4,11 +4,12 @@ function ComplexNumber(real, imaginary)
         Define and initialize the properties representing the real and imaginary components
         and the print function as it was defined in task 1
     */
-      
+    this.real = real;
+    this.imaginary = imaginary;
     this.print = function()
         {         
             // Please replace the comments with the numberic values only, don't change the printed string because it'll be used for testing
-            console.log(/*refer to the real part*/+" , " + /*refer to the imagenary part*/+ "i");
+            console.log(this.real+" , " + this.imaginary+ "i");
         }
 
     
@@ -20,7 +21,9 @@ function ComplexNumber(real, imaginary)
           Since it’s defined as a part of a complex number object itself, it has access to its own real and complex components (through the ‘this’ keyword).
            Create and return a new object that will contain the result of adding the received object to the current object. 
          */
-       
+       this.real= this.real + theOtherComplexNumber.real;
+       this.imaginary= this.imaginary + theOtherComplexNumber.imaginary;
+       return new ComplexNumber(this.real, this.imaginary);
     };
 
     this.subtract = function(theOtherComplexNumber)
@@ -28,15 +31,17 @@ function ComplexNumber(real, imaginary)
         /*
             Similar to the add method but the subtract operatin applied
         */
-       
+       this.real= this.real - theOtherComplexNumber.real;
+       this.imaginary= this.imaginary -theOtherComplexNumber.imaginary;
+       return new ComplexNumber(this.real, this.imaginary);
     };
 
     // the multiplication and the division are a little more involved, so they're not required
     // feel free to define them if you're interested; you won't be penalized if you don't
 };
 
-let c1; // use the constructor function to create the complex number 2 + 5i
-let c2; // use the constructor function to create the complex number 4 + 10i
+let c1= new ComplexNumber(2,5); // use the constructor function to create the complex number 2 + 5i
+let c2= new ComplexNumber(4,10); // use the constructor function to create the complex number 4 + 10i
 
 let c3 = c1.add(c2);
 c3.print();
